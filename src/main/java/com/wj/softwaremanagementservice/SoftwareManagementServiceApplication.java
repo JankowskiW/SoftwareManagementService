95e23ee1ac0e7ab53f9d288911d9080ddd4390dc
Waldemar <waldemar.jankowski95@gmail.com>
2024-09-11 13:19:16 +0200

    Init

---
+++ src/main/java/com/wj/softwaremanagementservice/SoftwareManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wj.softwaremanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wj/softwaremanagementservice/SoftwareManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wj.softwaremanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
