6b4356be72a6480342273169d25dc87376d76512
Waldemar <waldemar.jankowski95@gmail.com>
2024-09-16 22:37:28 +0200

    Create tests for ApplicationMapper

---
+++ src/test/java/com/wj/updatecenter/softwaremanagementservice/domain/application/mapper/ApplicationMapperTest.java
@@ -0,0 +1,179 @@
+package com.wj.updatecenter.softwaremanagementservice.domain.application.mapper;
+
+import com.wj.updatecenter.softwaremanagementservice.domain.application.model.Application;
+import com.wj.updatecenter.softwaremanagementservice.domain.application.model.dto.*;
+import com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.model.dto.GetApplicationVersionDetailsDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.wj.updatecenter.softwaremanagementservice.testhelper.ApplicationTestsHelper.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class ApplicationMapperTest {
+    @InjectMocks
+    private ApplicationMapper applicationMapper;
+
+    @Test
+    void shouldReturnNullInsteadOfGetApplicationDetailsDtoWhenApplicationIsNull() {
+        // given && when
+        GetApplicationDetailsDto result = applicationMapper.toGetApplicationDetailsDto(null, null);
+
+        // then
+        assertThat(result)
+                .isNull();
+    }
+
+    @Test
+    void shouldReturnGetApplicationDetailsDtoWithoutCurrentVersionRelatedFieldsWhenApplicationIsNotNullAndGetApplicationVersionDetailsDtoIsNull() {
+        // given
+        Application application = createDummyApplication(DUMMY_APPLICATION_ID);
+        GetApplicationDetailsDto expectedResult = createDummyGetApplicationDetailsDtoWithoutCurrentVersionFields(DUMMY_APPLICATION_ID);
+
+        // when
+        GetApplicationDetailsDto result = applicationMapper.toGetApplicationDetailsDto(application, null);
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldReturnGetApplicationDetailsDtoWithCurrentVersionRelatedFieldsWhenApplicationAndGetApplicationVersionDetailsDtoIsNotNull() {
+        // given
+        Application application = createDummyApplication(DUMMY_APPLICATION_ID);
+        GetApplicationVersionDetailsDto getApplicationVersionDetailsDto = createDummyGetApplicationVersionDetailsDto(1L, DUMMY_APPLICATION_ID);
+        GetApplicationDetailsDto expectedResult = createDummyGetApplicationDetailsDtoWithCurrentVersionFields(DUMMY_APPLICATION_ID);
+
+        // when
+        GetApplicationDetailsDto result = applicationMapper.toGetApplicationDetailsDto(application, getApplicationVersionDetailsDto);
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldReturnNullInsteadOfGetSimplifiedApplicationResponseDtoWhenApplicationIsNull() {
+        // given && when
+        GetSimplifiedApplicationResponseDto result = applicationMapper.toGetSimplifiedApplicationResponseDto(null);
+
+        // then
+        assertThat(result)
+                .isNull();
+    }
+
+    @Test
+    void shouldReturnGetSimplifiedApplicationResponseDtoWithoutCurrentVersionWhenCurrentVersionIsNull() {
+        // given
+        Application application = createDummyApplication(DUMMY_APPLICATION_ID);
+        application.setCurrentVersion(null);
+        GetSimplifiedApplicationResponseDto expectedResult =
+                createDummyGetSimplifiedApplicationResponseDtoWithoutCurrentVersion(DUMMY_APPLICATION_ID);
+
+        // when
+        GetSimplifiedApplicationResponseDto result = applicationMapper.toGetSimplifiedApplicationResponseDto(application);
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldReturnGetSimplifiedApplicationResponseDtoWithCurrentVersionWhenCurrentVersionIsNotNull() {
+        // given
+        Application application = createDummyApplication(DUMMY_APPLICATION_ID);
+        GetSimplifiedApplicationResponseDto expectedResult =
+                createDummyGetSimplifiedApplicationResponseDtoWithCurrentVersion(DUMMY_APPLICATION_ID);
+
+        // when
+        GetSimplifiedApplicationResponseDto result = applicationMapper.toGetSimplifiedApplicationResponseDto(application);
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldReturnNullInsteadOfApplicationWhenCreateApplicationRequestDtoIsNull() {
+        // given && when
+        Application result = applicationMapper.toApplication(null);
+
+        // then
+        assertThat(result)
+                .isNull();
+    }
+
+    @Test
+    void shouldReturnApplicationWhenCreateApplicationRequestDtoIsNotNull() {
+        // given
+        CreateApplicationRequestDto createApplicationRequestDto = createDummyCreateApplicationRequestDto();
+        Application expectedResult = createSimplyDummyApplication();
+
+        // when
+        Application result = applicationMapper.toApplication(createApplicationRequestDto);
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldReturnNullInsteadOfCreateApplicationResponseDtoWhenApplicationIsNull() {
+        // given && when
+        CreateApplicationResponseDto result = applicationMapper.toCreateApplicationResponseDto(null);
+
+        // then
+        assertThat(result)
+                .isNull();
+    }
+
+    @Test
+    void shouldReturnCreateApplicationResponseDtoWhenApplicationIsNotNull() {
+        // given
+        Application application = createDummyApplication(DUMMY_APPLICATION_ID);
+        CreateApplicationResponseDto expectedResult = createDummyCreateApplicationResponseDto(DUMMY_APPLICATION_ID);
+
+        // when
+        CreateApplicationResponseDto result = applicationMapper.toCreateApplicationResponseDto(application);
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldReturnNullInsteadOfApplicationWhenUpdateApplicationRequestDtoIsNull() {
+        // given && when
+        Application result = applicationMapper.toApplication(null, DUMMY_APPLICATION_ID);
+
+        // then
+        assertThat(result)
+                .isNull();
+    }
+
+    @Test
+    void shouldReturnApplicationWhenUpdateApplicationRequestDtoIsNotNull() {
+        // given
+        UpdateApplicationRequestDto updateApplicationRequestDto = createDummyUpdateApplicationRequestDto();
+        Application expectedResult = createSimplyDummyApplication();
+        expectedResult.setId(DUMMY_APPLICATION_ID);
+        expectedResult.setCurrentVersion(DUMMY_APPLICATION_CURRENT_VERSION);
+
+        // when
+        Application result = applicationMapper.toApplication(updateApplicationRequestDto, DUMMY_APPLICATION_ID);
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedResult);
+    }
+}
--- src/test/java/com/wj/updatecenter/softwaremanagementservice/testhelper/ApplicationTestsHelper.java
+++ src/test/java/com/wj/updatecenter/softwaremanagementservice/testhelper/ApplicationTestsHelper.java
@@ -1,6 +1,10 @@
 package com.wj.updatecenter.softwaremanagementservice.testhelper;
 
 import com.wj.updatecenter.softwaremanagementservice.domain.application.model.Application;
+import com.wj.updatecenter.softwaremanagementservice.domain.application.model.dto.*;
+import com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.model.dto.GetApplicationVersionDetailsDto;
+
+import java.time.LocalDateTime;
 
 public class ApplicationTestsHelper {
 
@@ -11,11 +15,96 @@ public class ApplicationTestsHelper {
     public static final String DUMMY_APPLICATION_DOCUMENTATION_URL = "ftp://192.168.0.1";
     public static final long DUMMY_APPLICATION_BUSINESS_OWNER_ID = 11L;
     public static final long DUMMY_APPLICATION_ASSIGNEE_ID = 21L;
-    public static final String DUMMY_APPLICATION_CURRENT_VERSION = "0.0.0.1";
+    public static final String DUMMY_APPLICATION_CURRENT_VERSION = "1.2.3.4";
     public static final long DUMMY_APPLICATION_CREATED_BY = 1L;
     public static final long DUMMY_APPLICATION_UPDATED_BY = 2L;
     public static final long DUMMY_APPLICATION_ARCHIVED_BY = 3L;
     public static final boolean DUMMY_APPLICATION_ARCHIVED = true;
+    public static final LocalDateTime DUMMY_APPLICATION_CREATED_AT = LocalDateTime.of(2024, 1, 23, 22, 22, 22);
+    public static final LocalDateTime DUMMY_APPLICATION_UPDATED_AT = LocalDateTime.of(2024, 1, 26, 23, 23, 23);
+    public static final LocalDateTime DUMMY_APPLICATION_ARCHIVED_AT = LocalDateTime.of(2024, 1, 26, 23, 23, 23);
+
+    public static final int DUMMY_VERSION_MAJOR = 1;
+    public static final int DUMMY_VERSION_MINOR = 2;
+    public static final int DUMMY_VERSION_PATCH = 3;
+    public static final int DUMMY_VERSION_BUILD = 4;
+    public static final String DUMMY_VERSION_FULL = DUMMY_APPLICATION_CURRENT_VERSION;
+    public static final boolean DUMMY_VERSION_CURRENT = true;
+    public static final String DUMMY_VERSION_CHANGELOG = "changelog";
+    public static final long DUMMY_VERSION_CREATED_BY = 1L;
+    public static final LocalDateTime DUMMY_VERSION_CREATED_AT = LocalDateTime.of(2024, 1, 27, 22, 22, 22);
+    public static final long DUMMY_VERSION_UPDATED_BY = 2L;
+    public static final LocalDateTime DUMMY_VERSION_UPDATED_AT = LocalDateTime.of(2024, 1, 27, 23, 23, 23);
+
+    public static UpdateApplicationRequestDto createDummyUpdateApplicationRequestDto() {
+        return new UpdateApplicationRequestDto(
+                DUMMY_APPLICATION_NAME,
+                DUMMY_APPLICATION_DESCRIPTION,
+                DUMMY_APPLICATION_REPOSITORY_URL,
+                DUMMY_APPLICATION_DOCUMENTATION_URL,
+                DUMMY_APPLICATION_BUSINESS_OWNER_ID,
+                DUMMY_APPLICATION_ASSIGNEE_ID,
+                DUMMY_APPLICATION_CURRENT_VERSION,
+                DUMMY_APPLICATION_ARCHIVED
+        );
+    }
+
+    public static CreateApplicationResponseDto createDummyCreateApplicationResponseDto(long id) {
+        return new CreateApplicationResponseDto(
+                id,
+                DUMMY_APPLICATION_NAME,
+                DUMMY_APPLICATION_DESCRIPTION,
+                DUMMY_APPLICATION_REPOSITORY_URL,
+                DUMMY_APPLICATION_DOCUMENTATION_URL,
+                DUMMY_APPLICATION_BUSINESS_OWNER_ID,
+                DUMMY_APPLICATION_ASSIGNEE_ID
+        );
+    }
+
+    public static CreateApplicationRequestDto createDummyCreateApplicationRequestDto() {
+        return new CreateApplicationRequestDto(
+                DUMMY_APPLICATION_NAME,
+                DUMMY_APPLICATION_DESCRIPTION,
+                DUMMY_APPLICATION_REPOSITORY_URL,
+                DUMMY_APPLICATION_DOCUMENTATION_URL,
+                DUMMY_APPLICATION_BUSINESS_OWNER_ID,
+                DUMMY_APPLICATION_ASSIGNEE_ID
+        );
+    }
+
+    public static GetSimplifiedApplicationResponseDto createDummyGetSimplifiedApplicationResponseDtoWithoutCurrentVersion(
+            long applicationId) {
+        return new GetSimplifiedApplicationResponseDto(
+                applicationId,
+                DUMMY_APPLICATION_NAME,
+                "");
+    }
+
+    public static GetSimplifiedApplicationResponseDto createDummyGetSimplifiedApplicationResponseDtoWithCurrentVersion(
+            long applicationId) {
+        return new GetSimplifiedApplicationResponseDto(
+                applicationId,
+                DUMMY_APPLICATION_NAME,
+                DUMMY_VERSION_FULL);
+    }
+
+    public static GetApplicationVersionDetailsDto createDummyGetApplicationVersionDetailsDto(long versionId, long applicationId) {
+        return new GetApplicationVersionDetailsDto(
+                versionId,
+                applicationId,
+                DUMMY_VERSION_MAJOR,
+                DUMMY_VERSION_MINOR,
+                DUMMY_VERSION_PATCH,
+                DUMMY_VERSION_BUILD,
+                DUMMY_VERSION_FULL,
+                DUMMY_VERSION_CURRENT,
+                DUMMY_VERSION_CHANGELOG,
+                DUMMY_VERSION_CREATED_BY,
+                DUMMY_VERSION_CREATED_AT,
+                DUMMY_VERSION_UPDATED_BY,
+                DUMMY_VERSION_UPDATED_AT
+        );
+    }
 
     public static Application createDummyApplication(long id) {
         Application application = new Application();
@@ -26,11 +115,69 @@ public class ApplicationTestsHelper {
         application.setDocumentationUrl(DUMMY_APPLICATION_DOCUMENTATION_URL);
         application.setBusinessOwnerId(DUMMY_APPLICATION_BUSINESS_OWNER_ID);
         application.setAssigneeId(DUMMY_APPLICATION_ASSIGNEE_ID);
-        application.setCurrentVersion(DUMMY_APPLICATION_CURRENT_VERSION);
+        application.setCurrentVersion(DUMMY_VERSION_FULL);
         application.setCreatedBy(DUMMY_APPLICATION_CREATED_BY);
+        application.setCreatedAt(DUMMY_APPLICATION_CREATED_AT);
         application.setUpdatedBy(DUMMY_APPLICATION_UPDATED_BY);
+        application.setUpdatedAt(DUMMY_APPLICATION_UPDATED_AT);
         application.setArchived(DUMMY_APPLICATION_ARCHIVED);
         application.setArchivedBy(DUMMY_APPLICATION_ARCHIVED_BY);
+        application.setArchivedAt(DUMMY_APPLICATION_ARCHIVED_AT);
         return application;
+    }
+
+    public static Application createSimplyDummyApplication() {
+        Application application = new Application();
+        application.setName(DUMMY_APPLICATION_NAME);
+        application.setDescription(DUMMY_APPLICATION_DESCRIPTION);
+        application.setRepositoryUrl(DUMMY_APPLICATION_REPOSITORY_URL);
+        application.setDocumentationUrl(DUMMY_APPLICATION_DOCUMENTATION_URL);
+        application.setBusinessOwnerId(DUMMY_APPLICATION_BUSINESS_OWNER_ID);
+        application.setAssigneeId(DUMMY_APPLICATION_ASSIGNEE_ID);
+        return application;
+    }
+
+    public static GetApplicationDetailsDto createDummyGetApplicationDetailsDtoWithoutCurrentVersionFields(long id) {
+        return new GetApplicationDetailsDto(
+                id,
+                DUMMY_APPLICATION_NAME,
+                DUMMY_APPLICATION_DESCRIPTION,
+                DUMMY_APPLICATION_REPOSITORY_URL,
+                DUMMY_APPLICATION_DOCUMENTATION_URL,
+                DUMMY_APPLICATION_BUSINESS_OWNER_ID,
+                DUMMY_APPLICATION_ASSIGNEE_ID,
+                DUMMY_APPLICATION_CREATED_BY,
+                DUMMY_APPLICATION_CREATED_AT,
+                DUMMY_APPLICATION_UPDATED_BY,
+                DUMMY_APPLICATION_UPDATED_AT,
+                DUMMY_APPLICATION_ARCHIVED_BY,
+                DUMMY_APPLICATION_ARCHIVED_AT,
+                DUMMY_APPLICATION_ARCHIVED,
+                null,
+                0L,
+                null
+        );
+    }
+
+    public static GetApplicationDetailsDto createDummyGetApplicationDetailsDtoWithCurrentVersionFields(long id) {
+        return new GetApplicationDetailsDto(
+                id,
+                DUMMY_APPLICATION_NAME,
+                DUMMY_APPLICATION_DESCRIPTION,
+                DUMMY_APPLICATION_REPOSITORY_URL,
+                DUMMY_APPLICATION_DOCUMENTATION_URL,
+                DUMMY_APPLICATION_BUSINESS_OWNER_ID,
+                DUMMY_APPLICATION_ASSIGNEE_ID,
+                DUMMY_APPLICATION_CREATED_BY,
+                DUMMY_APPLICATION_CREATED_AT,
+                DUMMY_APPLICATION_UPDATED_BY,
+                DUMMY_APPLICATION_UPDATED_AT,
+                DUMMY_APPLICATION_ARCHIVED_BY,
+                DUMMY_APPLICATION_ARCHIVED_AT,
+                DUMMY_APPLICATION_ARCHIVED,
+                DUMMY_VERSION_FULL,
+                DUMMY_VERSION_CREATED_BY,
+                DUMMY_VERSION_CREATED_AT
+        );
     }
 }
