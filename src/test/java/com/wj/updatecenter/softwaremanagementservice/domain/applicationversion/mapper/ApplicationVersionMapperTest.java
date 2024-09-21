84191d140475438ef42780949a296cac13590bac
Waldemar <waldemar.jankowski95@gmail.com>
2024-09-21 21:14:09 +0200

    Create tests for ApplicationVersionMapper

---
+++ src/test/java/com/wj/updatecenter/softwaremanagementservice/domain/applicationversion/mapper/ApplicationVersionMapperTest.java
@@ -0,0 +1,116 @@
+package com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.mapper;
+
+import com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.model.ApplicationVersion;
+import com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.model.dto.CreateApplicationVersionRequestDto;
+import com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.model.dto.CreateApplicationVersionResponseDto;
+import com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.model.dto.GetApplicationVersionDetailsDto;
+import com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.model.dto.GetSimplifiedApplicationVersionResponseDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.wj.updatecenter.softwaremanagementservice.testhelper.ApplicationVersionTestsHelper.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class ApplicationVersionMapperTest {
+    @InjectMocks
+    private ApplicationVersionMapper applicationVersionMapper;
+
+    @Test
+    void shouldReturnGetApplicationVersionDetailsDtoWhenApplicationVersionIsNotNull() {
+        // given
+        ApplicationVersion applicationVersion = createDummyApplicationVersion(DUMMY_VERSION_ID);
+        GetApplicationVersionDetailsDto expectedResult = createDummyGetApplicationVersionDetailsDto(DUMMY_VERSION_ID);
+
+        // when
+        GetApplicationVersionDetailsDto result = applicationVersionMapper.toGetApplicationVersionDetailsDto(applicationVersion);
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldReturnNullInsteadOfGetApplicationVersionDetailsDtoWhenApplicationVersionIsNull() {
+        // given && when
+        GetApplicationVersionDetailsDto result = applicationVersionMapper.toGetApplicationVersionDetailsDto(null);
+
+        // then
+        assertThat(result).isNull();
+    }
+
+    @Test
+    void shouldReturnGetSimplifiedApplicationVersionResponseDtoWhenApplicationVersionIsNotNull() {
+        // given
+        ApplicationVersion applicationVersion = createDummyApplicationVersion(DUMMY_VERSION_ID);
+        GetSimplifiedApplicationVersionResponseDto expectedResult = createDummyGetSimplifiedApplicationVersionResponseDto(DUMMY_VERSION_ID);
+
+        // when
+        GetSimplifiedApplicationVersionResponseDto result = applicationVersionMapper.toGetSimplifiedApplicationVersionResponseDto(applicationVersion);
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldReturnNullInsteadOfGetSimplifiedApplicationVersionResponseDtoWhenApplicationVersionIsNull() {
+        // given && when
+        GetSimplifiedApplicationVersionResponseDto result = applicationVersionMapper.toGetSimplifiedApplicationVersionResponseDto(null);
+
+        // then
+        assertThat(result).isNull();
+    }
+
+    @Test
+    void shouldReturnApplicationVersionWhenCreateApplicationVersionRequestDtoIsNotNull() {
+        // given
+        CreateApplicationVersionRequestDto createApplicationVersionRequestDto = createDummyCreateApplicationVersionRequestDto();
+        ApplicationVersion expectedResult = createSimplyApplicationVersion();
+
+        // when
+        ApplicationVersion result = applicationVersionMapper.toApplicationVersion(createApplicationVersionRequestDto, DUMMY_VERSION_ID);
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldReturnNullInsteadOfApplicationVersionWhenCreateApplicationVersionRequestDtoIsNull() {
+        // given && when
+        ApplicationVersion result = applicationVersionMapper.toApplicationVersion(null, DUMMY_VERSION_ID);
+
+        // then
+        assertThat(result).isNull();
+    }
+
+    @Test
+    void shouldReturnCreateApplicationVersionResponseDtoWhenApplicationVersionIsNotNull() {
+        // given
+        ApplicationVersion applicationVersion = createDummyApplicationVersion(DUMMY_VERSION_ID);
+        CreateApplicationVersionResponseDto expectedResult = createDummyCreateApplicationVersionResponseDto();
+
+        // when
+        CreateApplicationVersionResponseDto result = applicationVersionMapper.toCreateApplicationVersionResponseDto(applicationVersion);
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldReturnNullInsteadOfCreateApplicationVersionResponseDtoWhenApplicationVersionIsNull() {
+        // given && when
+        CreateApplicationVersionResponseDto result = applicationVersionMapper.toCreateApplicationVersionResponseDto(null);
+
+        // then
+        assertThat(result).isNull();
+    }
+}
--- src/test/java/com/wj/updatecenter/softwaremanagementservice/testhelper/ApplicationVersionTestsHelper.java
+++ src/test/java/com/wj/updatecenter/softwaremanagementservice/testhelper/ApplicationVersionTestsHelper.java
@@ -1,13 +1,14 @@
 package com.wj.updatecenter.softwaremanagementservice.testhelper;
 
+import com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.model.ApplicationVersion;
 import com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.model.dto.CreateApplicationVersionRequestDto;
 import com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.model.dto.CreateApplicationVersionResponseDto;
+import com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.model.dto.GetApplicationVersionDetailsDto;
 import com.wj.updatecenter.softwaremanagementservice.domain.applicationversion.model.dto.GetSimplifiedApplicationVersionResponseDto;
 
 import java.time.LocalDateTime;
 
 public class ApplicationVersionTestsHelper {
-    private static final long DUMMY_VERSION_ID = 1L;
     private static final long DUMMY_VERSION_APPLICATION_ID = 1L;
     private static final int DUMMY_VERSION_MAJOR = 5;
     private static final int DUMMY_VERSION_MINOR = 5;
@@ -16,11 +17,59 @@ public class ApplicationVersionTestsHelper {
     private static final boolean DUMMY_VERSION_CURRENT = true;
     private static final String DUMMY_VERSION_CHANGELOG = "changelog";
 
+    public static final long DUMMY_VERSION_ID = 1L;
     public static final long DUMMY_VERSION_CREATED_BY = 1L;
     public static final long DUMMY_VERSION_UPDATED_BY = 2L;
     public static final LocalDateTime DUMMY_VERSION_CREATED_AT = LocalDateTime.of(2024, 1, 23, 22, 22, 22);
     public static final LocalDateTime DUMMY_VERSION_UPDATED_AT = LocalDateTime.of(2024, 1, 26, 23, 23, 23);
 
+
+    public static GetApplicationVersionDetailsDto createDummyGetApplicationVersionDetailsDto(long id) {
+        return new GetApplicationVersionDetailsDto(
+                id,
+                DUMMY_VERSION_APPLICATION_ID,
+                DUMMY_VERSION_MAJOR,
+                DUMMY_VERSION_MINOR,
+                DUMMY_VERSION_PATCH,
+                DUMMY_VERSION_BUILD,
+                DUMMY_VERSION_MAJOR + "." +DUMMY_VERSION_MINOR + "." + DUMMY_VERSION_PATCH + "." + DUMMY_VERSION_BUILD,
+                DUMMY_VERSION_CURRENT,
+                DUMMY_VERSION_CHANGELOG,
+                DUMMY_VERSION_CREATED_BY,
+                DUMMY_VERSION_CREATED_AT,
+                DUMMY_VERSION_UPDATED_BY,
+                DUMMY_VERSION_UPDATED_AT
+        );
+    }
+
+    public static ApplicationVersion createSimplyApplicationVersion() {
+        ApplicationVersion applicationVersion = new ApplicationVersion();
+        applicationVersion.setApplicationId(DUMMY_VERSION_APPLICATION_ID);
+        applicationVersion.setMajor(DUMMY_VERSION_MAJOR);
+        applicationVersion.setMinor(DUMMY_VERSION_MINOR);
+        applicationVersion.setPatch(DUMMY_VERSION_PATCH);
+        applicationVersion.setBuild(DUMMY_VERSION_BUILD);
+        applicationVersion.setCurrent(DUMMY_VERSION_CURRENT);
+        applicationVersion.setChangelog(DUMMY_VERSION_CHANGELOG);
+        return applicationVersion;
+    }
+
+    public static ApplicationVersion createDummyApplicationVersion(long id) {
+        ApplicationVersion applicationVersion = new ApplicationVersion();
+        applicationVersion.setId(id);
+        applicationVersion.setApplicationId(DUMMY_VERSION_APPLICATION_ID);
+        applicationVersion.setMajor(DUMMY_VERSION_MAJOR);
+        applicationVersion.setMinor(DUMMY_VERSION_MINOR);
+        applicationVersion.setPatch(DUMMY_VERSION_PATCH);
+        applicationVersion.setBuild(DUMMY_VERSION_BUILD);
+        applicationVersion.setCurrent(DUMMY_VERSION_CURRENT);
+        applicationVersion.setChangelog(DUMMY_VERSION_CHANGELOG);
+        applicationVersion.setCreatedBy(DUMMY_VERSION_CREATED_BY);
+        applicationVersion.setCreatedAt(DUMMY_VERSION_CREATED_AT);
+        applicationVersion.setUpdatedBy(DUMMY_VERSION_UPDATED_BY);
+        applicationVersion.setUpdatedAt(DUMMY_VERSION_UPDATED_AT);
+        return applicationVersion;
+    }
 
     public static GetSimplifiedApplicationVersionResponseDto createDummyGetSimplifiedApplicationVersionResponseDto(
             long id) {
